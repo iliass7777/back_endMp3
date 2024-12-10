35022c31ab82d7523163df03168883abc443e34c
iliass7777 <iliasstranquille@gmail.com>
2024-12-10 17:27:57 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import { MP3 } from './mp3';
+
+function App() {
+  return (
+    <div className="App">
+      <MP3/>
+   
+    </div>
+  );
+}
+
+export default App;
---
+++ src/mp3/index.js
@@ -0,0 +1,6 @@
+import style from './index.module.css'
+export function MP3(){
+    return <div  className={style.container}>
+         <input type="text" className={style.input}/>
+    </div>
+}
