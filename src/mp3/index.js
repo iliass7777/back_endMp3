9377d1dd62771903ebb8b5e925f486d1eba34c90
iliass7777 <iliasstranquille@gmail.com>
2024-12-25 16:42:47 +0100

    finaly

--- src/mp3/index.js
+++ src/mp3/index.js
@@ -8,29 +8,28 @@ export function MP3() {
   const handleDownload = async () => {
     const url = in1.current.value;
     const formats = document.getElementsByName('format');
-    let format=''
+    let selectedFormat = '';
     if (!url) {
       setStatus('Please enter a valid URL.');
       return;
     }
-    for ( format of formats) {
+    for (const format of formats) {
       if (format.checked) {
-        format = String(format.value);
+        selectedFormat = String(format.value);
         break;
       }
     }
-    if (!format) {
+    if (!selectedFormat) {
       setStatus('Please select a format.');
       return;
     }
     try {
       console.log('Downloading MP3 from:', url);
       setStatus('Conversion initiée. Veuillez attendre...');
-      const { data } = await mp3(url, format); // Assuming `mp3` is a function that triggers the conversion
-      console.log(data)
+      const  {data}  = await mp3(url, selectedFormat); // Assuming `mp3` is a function that triggers the conversion
       const download = document.createElement("a");
-      download.href = "http://localhost:3008/ytverter/" + data;
-      format==="mp3"?download.download="download.mp3":download.download="download.mp4"
+      download.href = "http://localhost:3008/ytverter/" + Object.values(data)[0];
+      download.download = selectedFormat === "mp3" ? "download.mp3" : "download.mp4";
       download.click();
       setStatus('Téléchargement terminé');
     } catch (error) {
@@ -42,26 +41,26 @@ export function MP3() {
     setStatus('');
   };
 
-    return (
-      <>
-        <h1 className={style.h1}>Bienvenue MP3 Download</h1>
-        <div className={style.container}>
-          <input
-            type="text"
-            className={style.input}
-            ref={in1}
-            placeholder="Enter the video URL"
-            onChange={vider}
-          />
-          <button onClick={handleDownload}>Convertir</button>
-          <div>
-            <h3>{status}</h3>
-          </div>
-          <input type='radio' name='format' value='mp3' />mp3
-          <input type='radio' name='format' value='mp4' />mp4
+  return (
+    <>
+      <h1 className={style.h1}>Bienvenue MP3 Download</h1>
+      <div className={style.container}>
+        <input
+          type="text"
+          className={style.input}
+          ref={in1}
+          placeholder="Enter the video URL"
+          onChange={vider}
+        />
+        <button onClick={handleDownload}>Convertir</button>
+        <div>
+          <h3>{status}</h3>
         </div>
-      </>
-    );
+        <input type='radio' name='format' value='mp3' />mp3
+        <input type='radio' name='format' value='mp4' />mp4
+      </div>
+    </>
+  );
 }
 
 
